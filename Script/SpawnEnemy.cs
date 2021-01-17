36b16f5976ba5b9dac9b13185763f1e3c26f4466
Montagne94 <77510722+Montagne94@users.noreply.github.com>
2021-01-17 16:26:48 +0300

    Spawn Enemy

---
+++ Script/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private Transform[] _points;
+    void Start()
+    {
+        StartCoroutine(CreateEnemy());
+    }
+
+    private IEnumerator CreateEnemy()
+    {
+        int CurrentPoint = 0;
+        while (true)
+        {
+            Instantiate(_enemy, _points[CurrentPoint].transform.position, Quaternion.identity);
+            CurrentPoint++;
+            if(CurrentPoint >= _points.Length)
+            {
+                CurrentPoint = 0;
+            }
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
